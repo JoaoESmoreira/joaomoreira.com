fbb0cc4c20b939ca8dd1816394bd2c897841ae81
JoaoESmoreira <jesmoreira2010@gmail.com>
2025-10-28 11:41:32 +0000

    Add CC Licence and fixe compile warnings

--- build-site.el
+++ build-site.el
@@ -1,6 +1,27 @@
 
 (require 'ox-publish)
 
+(defun my/org-html-src-block (src-block _contents info)
+  (let* ((lang (org-element-property :language src-block))
+         (code (org-export-format-code-default src-block info))
+         ;; mapeamentos simples de Org → highlight.js
+         (hljs (pcase (downcase (or lang ""))
+                 ("emacs-lisp" "lisp")
+                 ("elisp" "lisp")
+                 ("shell" "bash")
+                 ("sh" "bash")
+                 ("bash" "bash")
+                 ("python" "python")
+                 (_ (downcase lang)))))
+    (format "<pre><code class=\"language-%s\">%s</code></pre>" (or hljs "") code)))
+
+(with-eval-after-load 'ox-html
+  (advice-add 'org-html-src-block :override #'my/org-html-src-block))
+
+(setq org-html-htmlize-output-type nil)
+
+
+
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
